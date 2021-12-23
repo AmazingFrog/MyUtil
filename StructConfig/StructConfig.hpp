3c1075991f45a6026cd4311ddcdecc87381f1f5f
黎愉铂 <liyubo961118@gmail.com>
2021-12-23 15:37:45 +0800

    增加用户自定义序列化方式

--- StructConfig/StructConfig.hpp
+++ StructConfig/StructConfig.hpp
@@ -2,35 +2,13 @@
 #define STRUCTCONFIG_H
 
 #include <string>
-
-#include "tinyxml2.h"
+#include <memory>
+#include <fstream>
+#include <algorithm>
+#include <unordered_map>
 
 namespace shochu
 {
-namespace toStrWapper {
-    constexpr unsigned int stringLeng = 32;
-    inline std::string toStr(bool i) {
-        return std::string(i ? "true" : "false");
-    }
-    inline std::string toStr(int i) {
-        char t[stringLeng] = { 0 };
-        sprintf(t, "%d", i);
-        return std::string(t);
-    }
-    inline std::string toStr(double i) {
-        char t[stringLeng] = { 0 };
-        sprintf(t, "%lf", i);
-        return std::string(t);
-    }
-    inline std::string toStr(float i) {
-        char t[stringLeng] = { 0 };
-        sprintf(t, "%lf", i);
-        return std::string(t);
-    }
-    inline std::string toStr(std::string i) {
-        return i;
-    }
-}; //toStrWapper
 namespace fromStrWapper {
     inline void fromStr(const std::string& s, bool& v) {
         v = (s == "true");
@@ -41,188 +19,329 @@ namespace fromStrWapper {
     inline void fromStr(const std::string& s, double& v) {
         sscanf(s.c_str(), "%lf", &v);
     }
-    inline void fromStr(const std::string& s, float& v) {
-        sscanf(s.c_str(), "%f", &v);
-    }
     inline void fromStr(const std::string& s, std::string& v) {
         v = s;
     }
 }; //fromStrWapper
+namespace toStrWapper {
+    template<typename T>
+    inline std::string toStr(const T& v) {
+        return std::to_string(v);
+    }
+    template<>
+    inline std::string toStr<std::string>(const std::string& s) {
+        return s;
+    }
+}
 
-template<int>
-struct uid {};
-
-template<typename T,typename... Args>
+template<typename T, typename... Args>
 struct isBase{};
 template<typename T>
 struct isBase<T> {
     using type = std::false_type;
     static constexpr bool value = type::value;
 };
-template<typename T,typename thisType,typename... Args>
-struct isBase<T,thisType,Args...> {
+template<typename T, typename thisType, typename... Args>
+struct isBase<T, thisType, Args...> {
     using type = typename std::conditional<std::is_same<T, thisType>::value, std::true_type, typename isBase<T, Args...>::type >::type;
     static constexpr bool value = type::value;
 };
-#define IsBaseType(type) shochu::isBase<type,int,double,float,bool,std::string>::value
+#define IsBaseType(type) shochu::isBase<type, int, double, bool, std::string>::value
+
+class SCNode {
+public:
+    SCNode() = default;
+    void key(const std::string& k) { this->k = k; }
+    void val(const std::string& v) { this->v = v; }
+    void appendChild(const SCNode& child) { std::string idx = child.attributes.at("uid"); this->childs.emplace(idx, child); }
+    void appendChild(SCNode&& child) { std::string idx = child.attributes.at("uid"); this->childs.emplace(std::move(idx), child); }
+    void setAttr(const std::string& k, const std::string& v) { this->attributes[k] = v; }
+    void setAttr(std::string&& k, std::string&& v) { this->attributes[k] = v; }
+    void isBase(bool i) { this->isB = i; }
+
+    typedef std::unordered_map<std::string, std::string>::const_iterator AttrIter;
+    typedef std::unordered_map<std::string, shochu::SCNode>::const_iterator ChildIter;
+    inline bool isBase() const { return this->isB; }
+    const std::string& key() const { return this->k; }
+    const std::string& val() const { return this->v; }
+    AttrIter attrBegin() const { return this->attributes.cbegin(); }
+    AttrIter attrEnd() const { return this->attributes.cend(); }
+    ChildIter childBegin() const { return this->childs.cbegin(); }
+    ChildIter childEnd() const { return this->childs.cend(); }
+
+private:
+    std::string k;
+    std::string v;
+    std::unordered_map<std::string, shochu::SCNode> childs;
+    std::unordered_map<std::string, std::string> attributes;
+    bool isB;
+
+    template<int N, typename T>
+    friend struct GetChildFromRoot;
+
+};
+
+class SCNodeFactoryInterface {
+public:
+    virtual std::string serialization(const SCNode& root) = 0;
+    virtual SCNode unserialization(const std::string& str) = 0;
+    virtual ~SCNodeFactoryInterface() {}
+
+};
+
+template<int>
+struct uid {};
 
 /**
-    从 nowElem 构造 val
+ * 将节点的值设置到对应成员
+ * cond 为是否是基本类型
 */
-template<bool cond,typename T>
+template<bool cond, typename T>
 struct ChooseSetValueFunc {};
 template<typename T>
-struct ChooseSetValueFunc<true,T> {
-    inline static void setValue(const tinyxml2::XMLElement* nowElem,T& val) {
-        shochu::fromStrWapper::fromStr(std::string(nowElem->GetText()), val);
+struct ChooseSetValueFunc<true, T> {
+    inline static void setValue(const SCNode& nowElem, T& val) {
+        shochu::fromStrWapper::fromStr(std::string(nowElem.val()), val);
     }
 };
 template<typename T>
-struct ChooseSetValueFunc<false,T> {
-    inline static void setValue(const tinyxml2::XMLElement* nowElem, T& val) {
-        val.fromXMLElement(nowElem);
+struct ChooseSetValueFunc<false, T> {
+    inline static void setValue(const SCNode& nowElem, T& val) {
+        val.fromSCNode(nowElem);
     }
 };
 
 /**
-    从 doc 创建xmlelement，xmlelement由 cls 和 thisUid 创建
+    从成员变成节点
+    cond 为是否是基本类型
 */
-template<bool cond, int N,typename outClass>
+template<bool cond, int N, typename outClass>
 struct ChooseGetValueFunc {};
 template<int N, typename outClass>
 struct ChooseGetValueFunc<true, N, outClass> {
-    inline static tinyxml2::XMLElement* getXMLFromVal(tinyxml2::XMLDocument* doc, shochu::uid<N> thisUid,outClass* cls) {
-        tinyxml2::XMLElement* nowElem = doc->NewElement(cls->getName(thisUid));
-        nowElem->SetAttribute("type", cls->getType(thisUid));
-
-        tinyxml2::XMLText* text = doc->NewText(toStrWapper::toStr(cls->getValue(thisUid)).c_str());
-        nowElem->InsertEndChild(text);
-
-        return nowElem;
+    inline static SCNode getValue(shochu::uid<N> thisUid, outClass* cls) {
+        SCNode n;
+        n.key(cls->getName(thisUid));
+        n.setAttr("type", cls->getType(thisUid));
+        n.setAttr("uid", shochu::toStrWapper::toStr(N));
+        n.val(toStrWapper::toStr(cls->getValue(thisUid)));
+        n.isBase(true);
+        return n;
     }
 };
 template<int N, typename outClass>
 struct ChooseGetValueFunc<false, N, outClass> {
-    inline static tinyxml2::XMLElement* getXMLFromVal(tinyxml2::XMLDocument* doc, shochu::uid<N> thisUid, outClass* cls) {
-        return cls->getValue(thisUid).toXMLElement(doc,true,cls->getName(thisUid));
+    inline static SCNode getValue(shochu::uid<N> thisUid, outClass* cls) {
+        SCNode n = cls->getValue(thisUid).toSCNode();
+        n.key(cls->getName(thisUid));
+        n.setAttr("type", cls->getType(thisUid));
+        n.setAttr("uid", shochu::toStrWapper::toStr(N));
+        n.isBase(false);
+        return n;
     }
 };
 
 /**
-    循环展开添加一个成员到root
+    循环展开 添加一个成员到root
 */
-template<int N,typename outClass>
+template<int N, typename outClass>
 struct AppendChildToRoot {
-    inline static void set(outClass* cls,tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* root) {
+    inline static void set(outClass* cls, shochu::SCNode& root) {
         shochu::uid<N> thisUid;
-        tinyxml2::XMLElement* nowElem = cls->getXMLElementFromValue(thisUid, doc);
-        nowElem->SetAttribute("type", cls->getType(thisUid));
-        nowElem->SetAttribute("uid", N);
-        root->InsertFirstChild(nowElem);
-        shochu::AppendChildToRoot<N - 1, outClass>::set(cls, doc, root);
+        root.appendChild(std::move(cls->val2SCNode(thisUid)));
+        shochu::AppendChildToRoot<N - 1, outClass>::set(cls, root);
     }
 };
 template<typename outClass>
 struct AppendChildToRoot<0, outClass> {
-    inline static void set(outClass* cls,tinyxml2::XMLDocument* doc, tinyxml2::XMLElement* root) {}
+    inline static void set(outClass* cls, shochu::SCNode& root) {}
 }; 
 
 /**
-    循环展开从root中获取一个element用来初始化成员
+    循环展开 从root中获取一个节点用来初始化成员
 */
-template<int N,typename outClass>
+template<int N, typename outClass>
 struct GetChildFromRoot {
-    inline static void get(outClass* cls, const tinyxml2::XMLElement* root){
+    inline static void get(outClass* cls, const SCNode& root) {
         shochu::uid<N> thisUid;
-        const tinyxml2::XMLElement* nowElem = root->FirstChildElement(cls->getName(thisUid));
-        cls->setValueByXMLElement(thisUid, nowElem);
-        shochu::GetChildFromRoot<N - 1, outClass>::get(cls,root);
+        auto idx = shochu::toStrWapper::toStr(N);
+        cls->SCNode2val(thisUid, root.childs.at(idx));
+        shochu::GetChildFromRoot<N - 1, outClass>::get(cls, root);
     }
 }; 
 template<typename outClass>
 struct GetChildFromRoot<0, outClass> {
-    inline static void get(outClass* cls, const tinyxml2::XMLElement* root) {}
+    inline static void get(outClass* cls, const SCNode& root) {}
 };
 
+// #define COMMENT(str) /\
+// /str
+#define COMMENT(str) 
+
 #define RegisterStruct_Begin(name)\
-struct name{\
+struct name {\
 private:\
     using outClass = name;\
-    enum { beg_menber = __COUNTER__ };\
-    tinyxml2::XMLDocument doc;
+    enum { beg_menber = __COUNTER__ };
 
-#define RegisterStruct_Menber(type,name)\
+#define RegisterStruct_Menber(type, name)\
 public:\
     type name;\
 private:\
     enum { beg_##name = __COUNTER__ - beg_menber};\
 \
-    constexpr auto getName(shochu::uid<beg_##name>){return #name;}\
-    constexpr auto getType(shochu::uid<beg_##name>) {return (IsBaseType(type))?#type:"struct:"#type;}\
-    auto& getValue(shochu::uid<beg_##name>) {return name;}\
-    const auto& getValue(shochu::uid<beg_##name>) const {return name;}\
+    COMMENT(当前成员的一些属性)\
+    constexpr auto getName(shochu::uid<beg_##name>) const {return #name;}\
+    constexpr auto getType(shochu::uid<beg_##name>) const {return (IsBaseType(type))?#type:"struct:"#type;}\
+    type& getValue(shochu::uid<beg_##name>) {return name;}\
+    const type& getValue(shochu::uid<beg_##name>) const {return name;}\
 \
-    void setValueByXMLElement(shochu::uid<beg_##name> thisUid,const tinyxml2::XMLElement* nowElem){\
-        shochu::ChooseSetValueFunc<IsBaseType(type),type>::setValue(nowElem,this->name);\
+    COMMENT(当前成员和SCNode的转换)\
+    void SCNode2val(shochu::uid<beg_##name> thisUid, const shochu::SCNode& n) {\
+        shochu::ChooseSetValueFunc<IsBaseType(type), type>::setValue(n, this->name);\
     }\
-    tinyxml2::XMLElement* getXMLElementFromValue(shochu::uid<beg_##name> thisUid,tinyxml2::XMLDocument* doc){\
-        return shochu::ChooseGetValueFunc<IsBaseType(type),(beg_##name),outClass>::getXMLFromVal(doc,thisUid,this);\
+    shochu::SCNode val2SCNode(shochu::uid<beg_##name> thisUid) {\
+        return shochu::ChooseGetValueFunc<IsBaseType(type), beg_##name, outClass>::getValue(thisUid, this);\
     }\
 \
-    friend struct shochu::AppendChildToRoot<beg_##name,outClass>;\
-    friend struct shochu::GetChildFromRoot<beg_##name,outClass>;\
-    friend struct shochu::ChooseSetValueFunc<IsBaseType(type),type>;\
-    friend struct shochu::ChooseGetValueFunc<IsBaseType(type),beg_##name,outClass>;
+    friend struct shochu::ChooseSetValueFunc<IsBaseType(type), type>;\
+    friend struct shochu::ChooseGetValueFunc<IsBaseType(type), beg_##name, outClass>;\
+    friend struct shochu::AppendChildToRoot<beg_##name, outClass>;\
+    friend struct shochu::GetChildFromRoot<beg_##name, outClass>;
 
 #define RegisterStruct_End(name)\
 private:\
-    const static size_t menberSize = __COUNTER__ - beg_menber - 1;\
+    constexpr static size_t menberSize = __COUNTER__ - beg_menber - 1;\
+    std::shared_ptr<shochu::SCNodeFactoryInterface> factory;\
 public:\
-    name() = default;\
-    name(const name& rhs){\
-        if(this == &rhs){\
-            return;\
-        }\
-        outClass* r = const_cast<outClass*>(&rhs);\
-        r->doc.DeepCopy(&(this->doc));\
-        this->fromXMLElement(this->doc.FirstChild()->ToElement());\
+    COMMENT(当前结构体和SCNode的转换)\
+    void fromSCNode(const shochu::SCNode& root) {\
+        shochu::GetChildFromRoot<menberSize, outClass>::get(this, root);\
     }\
-    name& operator=(const name& rhs){\
-        if(this == &rhs){\
-            return *this;\
-        }\
-        outClass* r = const_cast<outClass*>(&rhs);\
-        r->doc.DeepCopy(&(this->doc));\
-        this->fromXMLElement(this->doc.FirstChild()->ToElement());\
-        return *this;\
-    }\
-    inline tinyxml2::XMLElement* toXMLElement(tinyxml2::XMLDocument* document,bool isMenber,const char* menberName){\
-        tinyxml2::XMLElement* root = document->NewElement(isMenber?menberName:#name);\
-        shochu::AppendChildToRoot<menberSize,outClass>::set(this,document,root);\
+    shochu::SCNode toSCNode() {\
+        shochu::SCNode root;\
+        root.key(#name);\
+        root.isBase(false);\
+        shochu::AppendChildToRoot<menberSize, outClass>::set(this, root);\
         return root;\
     }\
-    inline void fromXMLElement(const tinyxml2::XMLElement* root){\
-        shochu::GetChildFromRoot<outClass::menberSize,outClass>::get(this,root);\
+public:\
+    name() : factory(new XMLFactory()) {}\
+    void setFactory(std::shared_ptr<shochu::SCNodeFactoryInterface> f) { this->factory = f; }\
+    void setFactory(shochu::SCNodeFactoryInterface* f) { this->factory.reset(f); }\
+    void saveToFile(const char* file) {\
+        std::fstream f;\
+        f.open(file, std::ios::out);\
+        if(f.is_open()) {\
+            auto s = this->factory->serialization(this->toSCNode());\
+            f.write(s.c_str(), s.size());\
+            f.close();\
+        }\
     }\
-    void saveToFile(const char* filename){\
-        this->doc.Clear();\
-        tinyxml2::XMLElement* root = this->toXMLElement(&(this->doc),false,"");\
-        this->doc.InsertEndChild(root);\
-        this->doc.SaveFile(filename);\
-    }\
-    void saveToFile(const std::string& f){\
-        this->saveToFile(f.c_str());\
-    }\
-    void loadFromFile(const char* filename){\
-        this->doc.Clear();\
-        this->doc.LoadFile(filename);\
-        this->fromXMLElement(this->doc.FirstChild()->ToElement());\
-    }\
-    void loadFromFile(const std::string& f){\
-        this->loadFromFile(f.c_str());\
+    void loadFromFile(const char* file) {\
+        std::fstream f;\
+        f.open(file, std::ios::in);\
+        if(f.is_open()) {\
+            std::string str;\
+            while(!f.eof()) {\
+                std::string s;\
+                std::getline(f, s);\
+                str.append(std::move(s));\
+            }\
+            f.close();\
+            this->fromSCNode(this->factory->unserialization(str));\
+        }\
     }\
 }; //RegisterStruct_Begin
 
 }; //shochu
 
-#endif // !STRUCTCONFIG_H
+// 默认使用tinyxml2
+#include "tinyxml2.h"
+class XMLFactory : public shochu::SCNodeFactoryInterface {
+public:
+    std::string serialization(const shochu::SCNode& root) {
+        auto r = this->doc.NewElement(root.key().c_str());
+        for(auto i=root.childBegin();i!=root.childEnd();++i) {
+            this->serializationHelper(i->second, r);
+        }
+        this->doc.InsertEndChild(r);
+        tinyxml2::XMLPrinter p;
+        this->doc.Print(&p);
+        return std::string(p.CStr());
+    }
+
+    shochu::SCNode unserialization(const std::string& str) {
+        doc.Parse(str.c_str());
+        auto root = doc.RootElement();
+
+        shochu::SCNode r;
+        r.key(root->Name());
+        r.isBase(false);
+        auto child = root->FirstChildElement();
+
+        while(child) {
+            shochu::SCNode sc = this->unserializationHelper(child);
+            r.appendChild(std::move(sc));
+            child = child->NextSiblingElement();
+        }
+
+        return r;
+    }
+
+private:
+    tinyxml2::XMLDocument doc;
+
+private:
+    void serializationHelper(const shochu::SCNode& root, tinyxml2::XMLElement* r) {
+        auto keyElem = this->doc.NewElement(root.key().c_str());
+
+        for(auto i=root.attrBegin();i!=root.attrEnd();++i) {
+            keyElem->SetAttribute(i->first.c_str(), i->second.c_str());
+        }
+
+        auto valElem = this->doc.NewText(root.val().c_str());
+        if(root.isBase()) {
+            keyElem->InsertEndChild(valElem);
+        }
+        else {
+            for(auto i=root.childBegin();i!=root.childEnd();++i) {
+                this->serializationHelper(i->second, keyElem);
+            }
+        }
+        r->InsertEndChild(keyElem);
+    }
+
+    shochu::SCNode unserializationHelper(tinyxml2::XMLElement* r) {
+        shochu::SCNode ret;
+
+        ret.key(r->Name());
+        auto attr = r->FirstAttribute();
+
+        // 设置属性
+        while(attr) {
+            ret.setAttr(std::string(attr->Name()), std::string(attr->Value()));
+            attr = attr->Next();
+        }
+
+        std::string typeStr = r->Attribute("type");
+        // 如果当前节点是一个结构体
+        if(typeStr.compare(0, sizeof("struct")-1, "struct") == 0) {
+            ret.isBase(false);
+            auto child = r->FirstChildElement();
+            while(child) {
+                ret.appendChild(std::move(this->unserializationHelper(child)));
+                child = child->NextSiblingElement();
+            }
+        }
+        else {
+            ret.isBase(true);
+            ret.val(r->FirstChild()->Value());
+        }
+
+        return ret;
+    }
+
+};
+
+#endif // STRUCTCONFIG_H
--- StructConfig/StructConfig.md
+++ StructConfig/StructConfig.md
@@ -1,25 +1,25 @@
-# StructString
+# StructConfig
 
 ## 简介
-提供saveToFile和loadFromFile接口,将配置类中的参数简单地保存到xml文件中
-算是实现了简单的反射(?
-xml使用的是[tinyxml2](https://github.com/leethomason/tinyxml2)
-基本类型目前只支持int,double,float,bool,std::string
+提供saveToFile和loadFromFile接口,将配置类中的参数简单地保存到文件中
+算是实现了简单的编译期反射(?
+基本类型目前只支持int,double,bool,std::string
 同时支持使用RegisterStruct宏注册的自定义结构体
 
 ## 用法
+1. 继承`shochu::SCNodeFactoryInterface`实现用户自定义的序列化方式，并通过`shochu::SCNode::setFactory`设置给指定的结构体，默认使用xml格式(xml使用的是[tinyxml2](https://github.com/leethomason/tinyxml2))
+
+1. 代码中定义结构体并使用
 ```c++
 RegisterStruct_Begin(Test)
 RegisterStruct_Menber(int, t)
 RegisterStruct_End(Test)
-
 
 RegisterStruct_Begin(MyStruct)
 RegisterStruct_Menber(int,i)
 RegisterStruct_Menber(Test,tt)
 RegisterStruct_Menber(double,d)
 RegisterStruct_End(MyStruct)
-
 
 MyStruct test;
 test.i = 10;
@@ -29,8 +29,10 @@ test.saveToFile("MyConfig.xml");
 
 MyStruct t2;
 t2.loadFromFile("MyConfig.xml");
+
 //use config
 ```
+
 ```xml
 <MyStruct>
     <i type="int" uid="1">10</i>
@@ -42,6 +44,10 @@ t2.loadFromFile("MyConfig.xml");
 
 ```
 
+## note
+1. 继承`shochu::SCNodeFactoryInterface`进行序列化时，要将`shochu::SCNode`的key, attr进行序列化，如果是基本类型的话，序列化val，否则遍历childs生成子节点添加到当前节点进行序列化
+1. 反序列化时，要设置`shochu::SCNode`的key, isBase, 如果是基本类型就设置val,否则构建子节点添加到当前节点的child
+
 ## TODO
 - [x] 支持更多类型(包括自定义类型)
-- [ ] 提供保存为json的功能
+- [x] 提供保存为json的功能(改为用户提供序列化方式)
