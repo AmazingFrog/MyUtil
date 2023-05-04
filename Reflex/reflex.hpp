cf1a171adb4b26c8939d8ac9480f30c0261bede4
magicShochu <liyubo961118@gmail.com>
2023-05-04 12:55:32 +0800

    添加test和反射

---
+++ Reflex/reflex.hpp
@@ -0,0 +1,192 @@
+#ifndef _REFLEX_H_
+#define _REFLEX_H_
+
+#include <any>
+#include <string>
+#include <memory>
+#include <utility>
+#include <iostream>
+#include <functional>
+#include <unordered_map>
+
+namespace shochu {
+using std::tuple;
+using std::string;
+using std::vector;
+
+struct FuncBase {
+    virtual void run(void* cls, void* res, const std::vector<std::any>& args)  = 0;
+    virtual void run(void* cls, const std::vector<std::any>& args) = 0;
+};
+
+template<typename Cls, typename Res, typename... Args>
+struct FuncMetadata : public FuncBase {
+    using R = Res;
+    using FuncType = std::function<Res(Cls*, Args...)>;
+    using ArgsType = tuple<Args...>;
+    using Class = Cls;
+    const static size_t ArgsSize = sizeof...(Args);
+
+    string name;
+    FuncType fn;
+
+    FuncMetadata() = delete;
+    FuncMetadata(string fnName, Res (Cls::*f)(Args...)) : name(fnName), fn(f) {}
+
+    void run(void* cls, void* res, const std::vector<std::any>& args) override {
+        if(ArgsSize != args.size()) {
+            return;
+        }
+
+        doRun<ArgsSize>(cls, res, args);
+    }
+    void run(void* cls, const std::vector<std::any>& args) override {
+        if(ArgsSize != args.size()) {
+            return;
+        }
+
+        doRun<ArgsSize>(cls, nullptr, args);
+    }
+
+    template<size_t M, typename... ArgType>
+    void doRun(void* cls, void* res, const std::vector<std::any>& args, ArgType... arg) {
+        if constexpr (M == 0) {
+            if constexpr (std::is_same_v<Res, void>) {
+                fn((Cls*)cls, arg...);
+            }
+            else {
+                *(Res*)res = fn((Cls*)cls, arg...);
+            }
+        }
+        else {
+            using thisArgType = std::tuple_element_t<M-1, ArgsType>;
+            try {
+                thisArgType thisArg = std::any_cast<thisArgType>(args[M-1]);
+                doRun<M-1, thisArgType, ArgType...>((Cls*)cls, res, args, thisArg, arg...);
+            }
+            catch(const std::bad_any_cast& e) {
+                std::cerr << "run fn [" << name << "], [" << M-1 <<"] arg type dismatch\n";
+            }
+        }
+    }
+};
+
+struct MemberBase {
+    virtual void getValue(void* cls, void* res) = 0;
+    virtual void setValue(void* cls, const void* v) = 0;
+    virtual void* get(void* cls) = 0;
+};
+
+template<typename Cls, size_t N, typename T>
+struct MemberMetadata : public MemberBase {
+    using Ty = T;
+    
+    Ty Cls::* p;
+    string name;
+    MemberMetadata(const char (&memName), T (Cls::* pmem)) : p(pmem), name(memName) {}
+
+    void getValue(void* cls, void* res) override {
+        if(res != nullptr) {
+            *(T*)res = ((Cls*)cls)->*p;
+        }
+    }
+    void setValue(void* cls, const void* v) override {
+        if(v != nullptr) {
+            ((Cls*)cls)->*p = *(const T*)v;
+        }
+    }
+    void* get(void* cls) {
+        return &(((Cls*)cls)->*p);
+    }
+};
+
+template<typename T>
+struct Reflex {
+    using FnMapType = std::unordered_map<string, std::unique_ptr<FuncBase>>;
+    using MemMapType = std::unordered_map<string, std::unique_ptr<MemberBase>>;
+
+    static FnMapType& fnMap() {
+        static FnMapType f;
+        return f;
+    }
+    static MemMapType& memberMap() {
+        static MemMapType m;
+        return m;
+    }
+
+    template<typename Res, typename... Args>
+    static void regFn(const string& fnName, Res(T::*f)(Args...)) {
+        fnMap().emplace(fnName, new FuncMetadata(fnName, f));
+    }
+
+    template<typename Res, typename... Args>
+    void runFn(T& cls, const string& fnName, Res* res, Args... args) {
+        auto fnIt = fnMap().find(fnName);
+        if(fnIt == fnMap().end()) {
+            std::cout << "don't find fn [" << fnName << "]\n";
+            return;
+        }
+
+        vector<std::any> arg{args...};
+        fnIt->second->run(&cls, res, arg);
+    }
+
+    template<typename... Args>
+    void runFn(T& cls, const string& fnName, Args... args) {
+        auto fnIt = fnMap().find(fnName);
+        if(fnIt == fnMap().end()) {
+            std::cout << "don't find fn [" << fnName << "]\n";
+            return;
+        }
+
+        vector<std::any> arg{args...};
+        fnIt->second->run(&cls, arg);
+    }
+
+    template<typename Mem>
+    void regMember(const string& mem, Mem T::*p) {
+        memberMap().emplace(mem, new MemberMetadata(mem, p));
+    }
+
+    template<typename R>
+    R* getMenber(T& cls, const string& memName) {
+        auto mem = memberMap().find(memName);
+        if(mem == memberMap().end()) {
+            std::cerr << "don't find member [" << memName << "]\n";
+            return nullptr;
+        }
+        return (R*)mem->second->get(&cls);
+    }
+
+    FuncBase* getFn(const string& fnName) {
+        auto fnIt = fnMap().find(fnName);
+        if(fnIt == fnMap().end()) {
+            std::cerr << "don't find fn [" << fnName << "]\n";
+            return nullptr;
+        }
+
+        return fnIt->second.get();
+    }
+
+    static void clear() {
+        fnMap().clear();
+        memberMap().clear();
+    }
+};
+
+#define REFLEX_BEG(cls)     \
+struct Reflex_##cls {       \
+    using T = cls;          \
+    Reflex_##cls() {\
+        cout << "con\n";
+
+#define REFLEX_FN(name, fn) shochu::Reflex<T>::regFn(name, fn);
+
+#define REFLEX_END(cls) \
+    }                   \
+};                      \
+static Reflex_##cls __reflex_##cls;
+
+}
+
+#endif // _REFLEX_H_
---
+++ test/test.hpp
@@ -0,0 +1,174 @@
+#ifndef _TEST_H_
+#define _TEST_H_
+
+#include <string>
+#include <vector>
+#include <variant>
+
+#include "reflex.hpp"
+
+namespace shochu {
+using std::string;
+using std::vector;
+using std::tuple;
+using std::cout;
+using std::to_string;
+
+static int TEST_CASE_NUM = 1;
+
+string to_string(const string& s) {
+    return "\"" + s + "\"";
+}
+
+string to_string(const char* s) {
+    return "\"" + string(s) + "\"";
+}
+
+string to_string(bool b) {
+    return b?"true":"false";
+}
+
+template<size_t N, typename... Args>
+struct Tuple2String {
+    static void to(const tuple<Args...>& a, string& s) {
+        if constexpr (N != sizeof...(Args)) {
+            s.append(to_string(std::get<N>(a)));
+            s.append(", ");
+            Tuple2String<N+1, Args...>::to(a, s);
+        }
+    }
+};
+
+template<typename... Args>
+string to_string(const tuple<Args...>& a) {
+    string s("(");
+    Tuple2String<0, Args...>::to(a, s);
+    s.pop_back();
+    s.pop_back();
+    s.push_back(')');
+    return s;
+}
+
+template<typename Res, typename FuncRet>
+concept hasOperatorEqual = requires(Res r, FuncRet f) {
+    r == f;
+};
+
+template<typename T>
+concept hasToString = requires(T a) {
+    { to_string(a) } -> std::same_as<string>;
+};
+
+template<typename T>
+concept hasIter = requires(T a) {
+    std::begin(a);
+    std::end(a);
+};
+
+template<typename T>
+requires hasToString<T>
+string printRes(const T& p) {
+    return to_string(p);
+}
+
+template<typename T>
+requires hasIter<T>
+string printRes(const T& p) {
+    if(std::begin(p) == std::end(p)) {
+        return "[]";
+    }
+    string s;
+    s.push_back('[');
+    for(auto&& i : p) {
+        s.append(printRes(i));
+        s.append(", ");
+    }
+    s.pop_back();
+    s.pop_back();
+    s.push_back(']');
+    return s;
+}
+
+template<typename T>
+struct getMenFnClass;
+
+template<typename Func, typename T>
+struct getMenFnClass<Func T::*>{
+    using type = T;
+};
+
+template<typename Func, typename Res, typename... Args>
+requires std::is_member_function_pointer_v<Func>
+void test(Func testFunc, Res res, Args... args) {
+    using namespace shochu;
+    string errStr;
+    typename getMenFnClass<Func>::type s;
+    clock_t beg = clock();
+    auto r = (s.*testFunc)(args...);
+    clock_t end = clock();
+    bool cmpRes = r == res;
+    if(!cmpRes) {
+        errStr = ", expect res is " + printRes(res) + " and calc res is " + printRes(r);
+    }
+    
+    cout << "test case [" << TEST_CASE_NUM++ << "]: result => "
+         << (cmpRes?"\033[32mok\033[0m":"\033[31mfialure\033[0m");
+    cout << ", time use: " << (end-beg)*1.0/CLOCKS_PER_SEC*1000 << "ms";
+    if(!cmpRes) {
+        cout << errStr;
+    }
+    cout << "\n";
+}
+
+template<typename Func, typename Res, typename... Args>
+void test(Func testFunc, Res res, Args... args) {
+    using namespace shochu;
+    string errStr;
+    clock_t beg = clock();
+    auto r = testFunc(args...);
+    clock_t end = clock();
+    bool cmpRes = r == res;
+    if(!cmpRes) {
+        errStr = ", expect res is " + printRes(res) + " and calc res is " + printRes(r);
+    }
+    
+    cout << "test case [" << TEST_CASE_NUM++ << "]: result => "
+         << (cmpRes?"\033[32mok\033[0m":"\033[31mfialure\033[0m");
+    cout << ", time use: " << (end-beg)*1.0/CLOCKS_PER_SEC*1000 << "ms";
+    if(!cmpRes) {
+        cout << errStr;
+    }
+    cout << "\n";
+}
+
+struct typeHolder {
+    virtual bool operator==(void* rhs) = 0;
+};
+
+template<typename T>
+struct typeHolderImpl : public typeHolder {
+    T val;
+    typeHolderImpl() = delete;
+    typeHolderImpl(T v) : val(v) {}
+
+    bool operator==(void* rhs) override {
+        return val == *(T*)rhs;
+    }
+};
+
+// template<typename T>
+// void test(T& cls, const vector<string>& cmds, const vector<std::any>& args, const vector<variant<int, double>>& res) {
+//     if(cmds.size() != args.size()) {
+//         std::cerr << "cmds and args dismatch\n";
+//         return;
+//     }
+
+//     Reflex<T> re;
+//     for(int i=0;i<cmds.size();++i) {
+//         re.getFn(cmds[i])->run(&cls, nullptr, args[i]);
+//     }
+// }
+
+
+}
+#endif // _TEST_H_
