ae417e09e2a218a85f8f72a3d90245251b6e7a9c
magicShochu <liyubo961118@gmail.com>
2023-06-12 00:50:29 +0800

    增加声明

--- concept.hpp
+++ concept.hpp
@@ -1,12 +1,20 @@
 #ifndef _CONCEPT_H_
 #define _CONCEPT_H_
 
-#include <concepts>
 #include <string>
+#include <concepts>
 #include <iterator>
 
 namespace shochu {
+
+namespace util {
+template<typename T>
+std::string to_string(const T&);
+}
+
 namespace concepts {
+using std::to_string;
+using shochu::util::to_string;
 
 template<typename T>
 concept hasToString = requires(T a) {
